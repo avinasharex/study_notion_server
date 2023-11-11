b003978ea06bac7ed6bcd6dd142454ac0b5f5bb1
avinasharex <avinash462470@gmail.com>
2023-11-11 10:23:44 +0530

    Added user model

---
+++ models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    acoountType:{
+        type: String,
+        enum: ["ADMIN","STUDENT","TEACHER"],
+        required: true
+    },
+    additionalDetails:{
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: "Profile"
+    },
+    courses:[
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "Profile"
+        }
+    ],
+    image: {
+        type: String,
+        required: true
+    },
+    courseProgress: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "CourseProgress"
+        }
+    ],
+    active:{
+        type: Boolean
+    },
+    approve:{
+        type: Boolean
+    }
+},{timestamps: true})
+
+const User = model("User",userSchema)
+export default  User
