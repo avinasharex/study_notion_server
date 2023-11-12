160ac04e74221ca6c2911081a24d973e316197dc
avinasharex <avinash462470@gmail.com>
2023-11-12 11:33:53 +0530

    Added course model

---
+++ models/course.model.js
@@ -0,0 +1,50 @@
+import mongoose, { mongo } from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+    courseName: {
+        type: String,
+        trim: true
+    },
+    courseDescription: {
+        type: String,
+        trim: true
+    },
+    Instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    whatYouWillLearn: {
+        type: String
+    },
+    courseContent: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section"
+        }
+    ],
+    ratingAndReviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RatingAndReview"
+        }
+    ],
+    price: {
+        type: Number
+    },
+    thumbnail: {
+        type: String
+    },
+    tag: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag"
+    },
+    studentsEnrolled: [{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    }]
+},{timestamps: true})
+
+const Course = model("Course",courseSchema)
+export default  Course
