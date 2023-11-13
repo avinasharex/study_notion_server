5c70cc42cff4c36da6732c1fa916ea9e0b774f85
avinasharex <avinash462470@gmail.com>
2023-11-13 09:28:58 +0530

    Added otp send functionality

--- models/otp.model.js
+++ models/otp.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mailSender from "../utils/mailSender";
 
 const otpSchema = new mongoose.Schema({
     email : {
@@ -15,6 +16,20 @@ const otpSchema = new mongoose.Schema({
         expires: 5*60
     }
 },{timestamps: true})
+
+async function sendVerificationEmail(email,otp){
+    try {
+        const mailResponse = await mailSender(email,"Verification email from Study Notion", otp)
+        console.log(mailResponse);
+    } catch (error) {
+        console.log("Error occured while sending mail",error);
+    }
+}
+
+otpSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email,this.otp)
+    next()
+})
 
 const OTP = model("OTP",otpSchema)
 export default  OTP
---
+++ utils/mailSender.js
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer"
+
+const mailSender = async(email,title,body)=>{
+    try {
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        })
+        await transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: email,
+            subject: title,
+            html: body
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default mailSender
