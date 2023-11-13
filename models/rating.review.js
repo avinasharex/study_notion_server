c9181ef7d06c373bb1307408930d0ec5c67864fc
avinasharex <avinash462470@gmail.com>
2023-11-13 08:40:12 +0530

    Added rating and review model

---
+++ models/rating.review.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from "mongoose";
+
+const ratingAndReviewSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true,
+        trim: true
+    }
+},{timestamps: true})
+
+const RatingAndReview = model("RatingAndReview",ratingAndReviewSchema)
+export default  RatingAndReview
