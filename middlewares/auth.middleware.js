5f5aefeac17105597440be56bb4b5f552aa9293d
avinasharex <avinash462470@gmail.com>
2023-11-15 09:06:45 +0530

    Write middlewares isLoggedIn and authorizedRoles

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const isLoggedIn = (req,res,next)=>{
+    const { token } = req.cookies
+
+    if (!token) {
+        return next(new AppError("Unauthenticated please login again", 400))
+    }
+    const userDetails = jwt.verify(token, process.env.JWT_SECRET)
+
+    req.user = userDetails
+
+
+    next();
+}
+
+const authorizedRoles = (...roles) => (req, res, next) => {
+    const currentUserRole = req.user.role
+
+    if (!roles.includes(currentUserRole)) {
+        return next(new AppError("You do not permssion to access ", 400))
+    }
+    next();
+}
+
+export {isLoggedIn,authorizedRoles}
